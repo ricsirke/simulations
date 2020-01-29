4bd38e4dd9c4fac081263909c36c218ed0cbde68
ricsirke <benericsirke@gmail.com>
2020-01-29 03:25:06 +0100

    adding live plots

--- random surface growth/amoebes.py
+++ random surface growth/amoebes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
@@ -9,7 +10,7 @@ class World:
         self.world = world
         self.empty_block_mark = 0
         self.amoebes = None
-
+        
     def get_neighs(self, pos):
         i = pos[0]
         j = pos[1]
@@ -46,24 +47,91 @@ class World:
         self.amoebes = amoebes
         for amoebe in amoebes:
             self.occupy_block_by_amoebe(amoebe.starting_pos, amoebe)
+            ###
+            amoebe.perimeter_lengths.append(len(amoebe.perimeters))
+            amoebe.block_lengths.append(len(amoebe.blocks))
+            ###
             
     def evolve_amoebes(self):
         for amoebe in self.amoebes:
             random_perimeter = amoebe.occupy_random_perimeter()
             if random_perimeter:
                 self.occupy_block_by_amoebe(random_perimeter, amoebe)
-                    
-    def visualise_amoebes_evolution(self, interval_ms=50, frames=50, output_filename=None):
-        fig = plt.figure(figsize=(10,10))
-        im = plt.imshow(self.world)
-
+            ###    
+            amoebe.perimeter_lengths.append(len(amoebe.perimeters))
+            amoebe.block_lengths.append(len(amoebe.blocks))
+            ###
+            
+    def visualise_amoebes_evolution(self, interval_ms=50, frames=50, output_filename=None, figsize=None, cmap=None):
+        fig = plt.figure()
+        gs = fig.add_gridspec(2,3)
+        
+        im_ax = fig.add_subplot(gs[:2, :2])
+        
+        perimeter_plot_ax = fig.add_subplot(gs[0,2])
+        blocks_plot_ax = fig.add_subplot(gs[1,2])
+        
+        im_ax.axis('off')
+        im = im_ax.imshow(self.world, cmap=cmap)
+        color_matrix = im.cmap(im.norm(im.get_array()))
+        for amoebe in self.amoebes:
+            i = amoebe.starting_pos[0]
+            j = amoebe.starting_pos[1]
+            amoebe.color = matplotlib.colors.to_hex(color_matrix[i,j])
+        
+        
+        perimeter_plots = []
+        blocks_plots = []
+        self.perimeter_max = 0
+        self.block_max = 0
+        for amoebe in self.amoebes:
+            perimeter_plot, = perimeter_plot_ax.plot(list(range(len(amoebe.perimeter_lengths))), amoebe.perimeter_lengths, color=amoebe.color, linewidth=2)
+            perimeter_plots.append(perimeter_plot)
+            blocks_plot, = blocks_plot_ax.plot(list(range(len(amoebe.block_lengths))), amoebe.block_lengths, color=amoebe.color, linewidth=2)
+            blocks_plots.append(blocks_plot)
+            
+            if amoebe.perimeter_lengths[-1] > self.perimeter_max:
+                self.perimeter_max = amoebe.perimeter_lengths[-1]
+            if amoebe.block_lengths[-1] > self.block_max:
+                self.block_max = amoebe.block_lengths[-1]
+            
+        perimeter_plot_ax.set_xlim([0, frames])
+        perimeter_plot_ax.set_ylim([0, self.perimeter_max*1.1])
+        perimeter_plot_ax.set_xlabel('time')
+        perimeter_plot_ax.set_ylabel('perimeters')
+        
+        blocks_plot_ax.set_xlim([0, frames])
+        blocks_plot_ax.set_ylim([0, self.block_max*1.1])
+        blocks_plot_ax.set_xlabel('time')
+        blocks_plot_ax.set_ylabel('blocks')
+        
+        plt.tight_layout()
+        
         def animate(i):
             self.evolve_amoebes()
+            
             im.set_array(self.world)
             
+            for i, perimeter_plot in enumerate(perimeter_plots):
+                xdata_perimeter = list(range(len(self.amoebes[i].perimeter_lengths)))
+                perimeter_plot.set_xdata(xdata_perimeter)
+                perimeter_plot.set_ydata(self.amoebes[i].perimeter_lengths)
+                
+                xdata_blocks = list(range(len(self.amoebes[i].block_lengths)))
+                blocks_plots[i].set_xdata(xdata_blocks)
+                blocks_plots[i].set_ydata(self.amoebes[i].block_lengths)
+                
+                if self.amoebes[i].perimeter_lengths[-1] > self.perimeter_max:
+                    self.perimeter_max = self.amoebes[i].perimeter_lengths[-1]
+                    
+                if self.amoebes[i].block_lengths[-1] > self.block_max:
+                    self.block_max = self.amoebes[i].block_lengths[-1]
+            
+            perimeter_plot_ax.set_ylim([0, self.perimeter_max*1.1])
+            blocks_plot_ax.set_ylim([0, self.block_max*1.1])
+            
+            
         anim = FuncAnimation(fig, animate, frames=frames, interval=interval_ms, repeat=False)
-
-        plt.axis('off')
         
         if output_filename:
             anim.save(output_filename)
@@ -79,6 +147,9 @@ class Amoebe:
         self.starting_pos = starting_pos
         self.blocks = [self.starting_pos]
         self.perimeters = self.world.get_new_free_neighs(self.starting_pos, [])
+        
+        self.perimeter_lengths = [len(self.perimeters)]
+        self.block_lengths = [len(self.blocks)]
         
         if mark == self.world.empty_block_mark:
             raise Exception
